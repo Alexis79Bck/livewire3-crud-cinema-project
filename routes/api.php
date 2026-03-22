98f5ce9529b0a631061e6fff0ce90c0d2484393f
Alexis79Bck <alexedmata@gmail.com>
2026-03-22 14:24:56 -0400

    Agregar configuración de Sanctum y migración para tokens de acceso personal

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Catalog\MovieController;
+
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+    })->middleware('auth:sanctum');
+Route::prefix('catalog')->group(function () {
+    Route::prefix('movies')->group(function () {
+        Route::post('/', [MovieController::class, 'store']);
+        Route::patch('/{id}/publish', [MovieController::class, 'publish']);
+        Route::patch('/{id}/archive', [MovieController::class, 'archive']);
+    
+    });
+    
+});
+    
